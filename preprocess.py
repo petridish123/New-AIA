8143875249fedf2b22dcf338a5097e84671f208b
stevehenny <stephen.henstrom@gmail.com>
2024-03-02 17:14:01 -0700

    comments

--- preprocess.py
+++ preprocess.py
@@ -16,17 +16,13 @@ print('test')
 #                                                      (0.5, 0.5, 0.5))])
 # dataset = datasets.ImageFolder(root='bev_classification', transform=transform)
 
-
-gpus = keras.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    keras.config.experimental.set_memory_growth(gpu, True)
-
 data = tf.keras.utils.image_dataset_from_directory( 'bev_classification', labels='inferred', label_mode='int', class_names=None, color_mode='rgb', batch_size=32, image_size=(128, 256), shuffle=True, seed=123, validation_split=None, subset=None, interpolation='bilinear', follow_links=False)
 
 data = data.map(lambda x, y: (x/255, y))
 data_iter = data.as_numpy_iterator()
 batch = data_iter.next()
 
+#partition the data into train, validation, and test sets
 train_images = int(len(data) * 0.7)
 val_images = int(len(data) * 0.2)+1
 test_images = int(len(data) * 0.1)+1
@@ -36,24 +32,30 @@ val = data.skip(train_images).take(val_images)
 test = train_images.skip(val_images+train_images).take(val_images)
 
 
-
+#CNN model
 model = Sequential()
+#first layer with 16 filters, each filter being a 3x3 matrix
 model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(128, 256, 3)))
 model.add(MaxPooling2D())
 
+#second layer with 32 filters, each filter being a 3x3 matrix
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D())
 
 model.add(Flatten())
 
+#dense layer with 128 neurons
 model.add(Dense(128, activation='relu'))
+
+#output layer
 model.add(Dense(1, activation='softmax'))
 
-
+# calculate loss and accuracy
 model.compile(optimizer='adam', loss=keras.losses.binary_crossentropy(), metrics=['accuracy'])
 
 logdir = "logs"
 
+#tensorboard callback
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
 hist = model.fit(train, validation_data=val, epochs=20, callbacks=[tensorboard_callback])
